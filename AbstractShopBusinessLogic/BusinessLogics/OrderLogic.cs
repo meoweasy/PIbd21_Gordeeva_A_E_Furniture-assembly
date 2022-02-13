e622ab7ad4b62a47f8fc5aaf2af4614a75a9fcca
Gordeeva <meowweasy@mail.ru>
2022-02-13 19:13:50 +0400

    1

--- AbstractShopBusinessLogic/BusinessLogics/OrderLogic.cs
+++ AbstractShopBusinessLogic/BusinessLogics/OrderLogic.cs
@@ -7,6 +7,7 @@ using AbstractShopContracts.BindingModels;
 using AbstractShopContracts.BusinessLogicsContracts;
 using AbstractShopContracts.StoragesContacts;
 using AbstractShopContracts.ViewModels;
+using AbstractShopContracts.Enums;
 
 namespace AbstractShopBusinessLogic.BusinessLogics
 {
@@ -24,11 +25,48 @@ namespace AbstractShopBusinessLogic.BusinessLogics
             {
                 return _orderStorage.GetFullList();
             }
-            if (model.Id.HasValue)
-            {
-                return new List<ComponentViewModel> { _orderStorage.GetElement(model) };
-            }
             return _orderStorage.GetFilteredList(model);
+        }
+
+        public void UpdateStatus(OrderBindingModel model)
+        {
+            var element = _orderStorage.GetElement(new OrderBindingModel
+            {
+                Id = model.Id
+            });
+
+            if (element.Id == model.Id && element.Status == "Принят")
+            {
+                model.Status = OrderStatus.Выполняется;
+            }
+
+            if (element.Id == model.Id && element.Status == "Выполняется")
+            {
+                model.Status = OrderStatus.Готов;
+            }
+
+            if (element.Id == model.Id && element.Status == "Готов")
+            {
+                model.Status = OrderStatus.Выдан;
+            }
+        }
+
+        public void CreateOrder(CreateOrderBindingModel model)
+        {
+            var element = _orderStorage.GetElement(new OrderBindingModel
+            {
+                Count = model.Count,
+                Sum = model.Sum
+            });
+            if (element != null)
+            {
+                throw new Exception("Уже есть такой заказ");
+            }
+            else
+            {
+                _orderStorage.Insert();
+            }
+
         }
     }
 }
